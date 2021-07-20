1448c26292893e170cc85f8b93d32900a718f938
Asura <aravind1808@gmail.com>
2021-07-20 23:07:58 +0530

    Frontend coded with metamask

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Web3 from "web3";
+import { useState, Fragment } from "react";
+import { authAxios } from "./utils/axios";
 
 function App() {
+  const [getErrorMessage, setErrorMessage] = useState("");
+  const [getJWTToken, setJWTToken] = useState("");
+
+  const handleSignin = async () => {
+    setErrorMessage("");
+    setJWTToken("");
+    try {
+      //Nonce to serve as message to be signed
+      const { data: nonce } = await authAxios.get("/token");
+
+      //Check to see if Metamask is installed
+      if (!window.ethereum) {
+        setErrorMessage(
+          "No wallet found try https://metamask.io/download.html and set an account!"
+        );
+        return;
+      }
+      //Get an account from Metamask
+      const address = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      if (address.length < 0) {
+        setErrorMessage("No account found. Please, configure an account");
+      }
+      const web3 = new Web3(Web3.givenProvider);
+
+      //Signs data to unlock account
+      const signature = await web3.eth.personal.sign(
+        nonce.toString(),
+        address[0]
+      );
+
+      //Authenticate user and return a token
+      const { data: responseToken } = await authAxios.post("/auth", {
+        address: address[0],
+        signature,
+        nonce,
+      });
+
+      setJWTToken(responseToken);
+    } catch (error) {
+      setErrorMessage(error.message);
+      // console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <p> Click the below button to proceed</p>
+      <button onClick={() => handleSignin()} className="btn btn-primary">
+        {"Sign in"}
+      </button>
+      <p>{getErrorMessage ? getErrorMessage : null} </p>
+      <p>{getJWTToken ? `JWT Auth token: ${getJWTToken}` : null} </p>
+    </Fragment>
   );
 }
 
